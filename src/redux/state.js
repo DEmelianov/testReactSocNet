6b7107a9d69628192d6fd4b93c7a93a1f24c7eee
Daemoon-72 <daemoon72@gmail.com>
2021-10-26 19:03:42 +0500

    1

---
+++ src/redux/state.js
@@ -0,0 +1,37 @@
+let rerenderEntireTree = () => {
+  console.log('State changed')
+}
+
+let state = {
+  profilePage: {
+    posts: [
+      {id: 1, message: 'Hi, how are you?', likesCount: 12},
+      {id: 2, message: 'It\'s my first post', likesCount: 11},
+      {id: 3, message: 'Blabla', likesCount: 11},
+      {id: 4, message: 'Dada', likesCount: 11},
+    ],
+    newPostText: 'The new post text'
+  },
+  dialogsPage: {
+    dialogs: [
+      {id: 1, name: 'Alex'},
+      {id: 2, name: 'Alex2'},
+      {id: 3, name: 'Alex3'},
+      {id: 4, name: 'Alex4'},
+      {id: 5, name: 'Alex5'},
+    ],
+    messages: [
+      {id: 1, message: 'Mess1'},
+      {id: 2, message: 'Mess2'},
+      {id: 3, message: 'Mess3'},
+      {id: 4, message: 'Mess4'},
+    ]
+  },
+  sidebar: {}
+}
+
+window.state = state;
+
+export const addPost = () => {
+
+}
