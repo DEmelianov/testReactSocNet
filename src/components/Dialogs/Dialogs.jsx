7905ea337cdd138e80a75f714dae473cb83145a5
Daemoon-72 <daemoon72@gmail.com>
2021-10-26 23:07:16 +0500

    3

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,25 @@
+import s from './Dialogs.module.css'
+import DialogItem from './DialogItem/DialogItem'
+import Message from './Message/Message'
+
+const Dialogs = (props) => {
+  let dialogsElements = props.state.dialogs.map(
+    dialog => <DialogItem name={dialog.name} id={dialog.id}/>
+  )
+  // TODO: сделать мап прямо в ретурне
+  let messagesElements = props.state.messages.map(
+    message => <Message message={message.message}/>
+  )
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        {dialogsElements}
+      </div>
+      <div className={s.message}>
+        {messagesElements}
+      </div>
+    </div>
+  )
+}
+
+export default Dialogs
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/dialogs" activeClassName={s.activeLink}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={`${s.item} ${s.activeLink}`}>
+        <NavLink to="/dialogs" activeClassName={s.activeLink}>
+          Messages
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <a href="#">
+          News
+        </a>
+      </div>
+      <div className={s.item}>
+        <a href="#">
+          Music
+        </a>
+      </div>
+      <div className={s.item}>
+        <a href="#">
+          Settings
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
