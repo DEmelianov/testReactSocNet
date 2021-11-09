80cf64f97768e93980b9b0b782098a4d7b24267c
Daemoon-72 <daemoon72@gmail.com>
2021-11-09 16:58:53 +0500

    60

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
           }
         />
         <Route
-          path="/profile"
+          path="/profile/:userId?"
           render={() =>
             <ProfileContainer/>
           }
