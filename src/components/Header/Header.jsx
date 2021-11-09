49122db914aad72641c39df66aadc9816ed028ee
Daemoon-72 <daemoon72@gmail.com>
2021-11-10 01:28:24 +0500

    61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img src="https://static.wikia.nocookie.net/brawlhalla_gamepedia/images/0/00/Avatar_Silly_Face.png" alt=""/>
+      <div className={s.loginBlock}>
+        {
+          !props.isAuth ?
+            <NavLink to="/login">
+              Login
+            </NavLink> :
+            props.login
+        }
+      </div>
     </header>
   )
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reduser";
 import dialogsReducer from "./dialogs-reduser";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer"
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 })
 
 let store = createStore(reducers)
