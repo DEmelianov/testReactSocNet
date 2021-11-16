af5b3234f2e391bf447b07a6155a94471cdd9c43
Daemoon-72 <daemoon72@gmail.com>
2021-11-16 11:55:30 +0500

    71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloder";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -7,12 +8,12 @@ const ProfileInfo = (props) => {
   }
   return (
     <div>
-      <div>
-        <img alt="" src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350" />
-      </div>
+      {/*<div>*/}
+      {/*  <img alt="" src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350" />*/}
+      {/*</div>*/}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} alt=""/>
-        ava + description
+        <ProfileStatus status="Hi !!"/>
       </div>
     </div>
   )
